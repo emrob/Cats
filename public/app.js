02ae0dbfe196bb3e5fd9ed196c377675ae5dc5b8
Emma Roberts <emrsixtyseven@gmail.com>
2018-04-09 18:22:53 +0100

    refactored - but image not currently showing

--- public/app.js
+++ public/app.js
@@ -1,25 +1,79 @@
-const app = function(){
-  let catList = document.createElement("ul");
-  catList.classList.add("cat");
+// const app = function(){
+//   let catList = document.createElement("ul");
+//   catList.classList.add("cat");
+//
+//   let listName = document.createElement("listName");
+//   listName.innerHTML = '<li>Name: Mildred</li>';
+//
+//   let listFood = document.createElement("listFood");
+//   listFood.innerHTML = '<li>Favourite Food: Fish fingers</li>';
+//
+//   let listImg = document.createElement("listImg");
+  // listImg.innerHTML = '<li><img width="500" src="http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"></li>';
+//
+//   listName.appendChild(listFood);
+//   listName.appendChild(listImg);
+//
+//   catList.appendChild(listName);
+//
+//   let cats = document.querySelector("#cats");
+//   cats.appendChild(catList);
+//
+//
+// }
+//
+// window.onload = app;
 
-  let listName = document.createElement("listName");
-  listName.innerHTML = '<li>Name: Mildred</li>';
 
-  let listFood = document.createElement("listFood");
-  listFood.innerHTML = '<li>Favourite Food: Fish fingers</li>';
+// refactored:
+ const createCatList = function(){
+   let catList = document.createElement("ul");
+   catList.classList.add("cat");
+   return catList;
+ }
 
-  let listImg = document.createElement("listImg");
-  listImg.innerHTML = '<li><img width="500" src="http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"></li>';
+ const createName = function(name){
+   let listName = document.createElement("listName");
+   listName.innerHTML = name;
+   return listName
+}
 
+ const createFavFood = function(favFood){
+ let listFood = document.createElement("listFood");
+ listFood.innerHTML = favFood;
+ return listFood;
+}
+
+ const createImg = function(img){
+ let listImg = document.createElement("listImg");
+ listImg.innerHTML = "<li>img</li>";
+ return listImg;
+}
+
+const appendElements = function(catList, listName, listFood, listImg){
   listName.appendChild(listFood);
   listName.appendChild(listImg);
-
   catList.appendChild(listName);
-
   let cats = document.querySelector("#cats");
   cats.appendChild(catList);
 
+}
+
+const addCat = function(name, favFood, img) {
+  const catList = createCatList();
+  const listName = createName(name);
+  const listFood = createFavFood(favFood);
+  const listImg = createImg(img);
+
+  appendElements(catList, listName, listFood, listImg);
+}
+
+const app = function(){
+
+addCat("<li>Name: Mr Pickles<li>", "<li>Favourite Food: Fish fingers</li>",
+'<li>"http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"</li>');
 
 }
+
 
 window.onload = app;
