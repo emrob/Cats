2c8fead282de8fd341e7f94ccc4f56698071c1c7
Emma Roberts <emrsixtyseven@gmail.com>
2018-04-09 22:04:23 +0100

    refactored - but image not currently showing

--- public/app.js
+++ public/app.js
@@ -27,53 +27,51 @@
 
 // refactored:
  const createCatList = function(){
-   let catList = document.createElement("ul");
-   catList.classList.add("cat");
-   return catList;
+   let ul = document.createElement("ul");
+   ul.classList.add("cat");
+   return ul;
  }
 
  const createName = function(name){
-   let listName = document.createElement("listName");
+   let listName = document.createElement("li");
    listName.innerHTML = name;
    return listName
 }
 
  const createFavFood = function(favFood){
- let listFood = document.createElement("listFood");
+ let listFood = document.createElement("li");
  listFood.innerHTML = favFood;
  return listFood;
 }
 
- const createImg = function(img){
- let listImg = document.createElement("listImg");
- listImg.innerHTML = "<li>img</li>";
+ const createImg = function(photo){
+ let listImg = document.createElement("li");
+ listImg.innerHTML = "<img width='500' src=photo >";
  return listImg;
 }
 
-const appendElements = function(catList, listName, listFood, listImg){
+
+const appendElements = function(ul, listName, listFood, listImg){
   listName.appendChild(listFood);
   listName.appendChild(listImg);
-  catList.appendChild(listName);
+  ul.appendChild(listName);
   let cats = document.querySelector("#cats");
-  cats.appendChild(catList);
+  cats.appendChild(ul);
 
 }
 
-const addCat = function(name, favFood, img) {
-  const catList = createCatList();
+const addCat = function(name, favFood, photo) {
+  const ul = createCatList();
   const listName = createName(name);
   const listFood = createFavFood(favFood);
-  const listImg = createImg(img);
+  const listImg = createImg(photo);
 
-  appendElements(catList, listName, listFood, listImg);
-}
-
-const app = function(){
-
-addCat("<li>Name: Mr Pickles<li>", "<li>Favourite Food: Fish fingers</li>",
-'<li>"http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"</li>');
-
+  appendElements(ul, listName, listFood, listImg);
 }
 
 
-window.onload = app;
+window.onload = function(){
+addCat("Name: Mr Pickles", "Favourite Food: Fish fingers",
+"http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg");
+
+};
